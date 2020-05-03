7401ced5ecc9b37649ed209f276d898db70e0ec4
Vinay Mathew John <vinay.john@litmus7.com>
2020-05-03 15:06:25 +0530

    Designing Tests for Utility Classes - 6 for Login Use Case Sync + Code Refactor

--- app/src/test/java/vinay/com/simplecalcforunittesting/logintestdoubles/LoginUseCaseSyncTest.java
+++ app/src/test/java/vinay/com/simplecalcforunittesting/logintestdoubles/LoginUseCaseSyncTest.java
@@ -7,9 +7,11 @@ import org.junit.Test;
 
 import vinay.com.simplecalcforunittesting.logintestdoubles.authtoken.AuthTokenCache;
 import vinay.com.simplecalcforunittesting.logintestdoubles.eventbus.EventBusPoster;
+import vinay.com.simplecalcforunittesting.logintestdoubles.eventbus.LoggedInEvent;
 import vinay.com.simplecalcforunittesting.logintestdoubles.networking.LoginHttpEndpointSync;
 import vinay.com.simplecalcforunittesting.logintestdoubles.networking.NetworkErrorException;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
@@ -23,14 +25,16 @@ public class LoginUseCaseSyncTest {
     LoginHttpEndpointSyncTestDouble mLoginHttpEndpointSyncTestDouble;
     AuthTokenCacheTestDouble mAuthTokenCacheTestDouble;
     LoginUseCaseSync mLoginUseCaseSync;
+    EventBusPosterTestDouble mEventBusPosterTestDouble;
 
     @Before
     public void setUp() throws Exception {
         mLoginHttpEndpointSyncTestDouble = new LoginHttpEndpointSyncTestDouble();
         mAuthTokenCacheTestDouble = new AuthTokenCacheTestDouble();
+        mEventBusPosterTestDouble = new EventBusPosterTestDouble();
         mLoginUseCaseSync = new LoginUseCaseSync(mLoginHttpEndpointSyncTestDouble,
                 mAuthTokenCacheTestDouble,
-                new EventBusPosterTestDouble());
+                mEventBusPosterTestDouble);
     }
 
     //username and password passed to the endpoint
@@ -50,39 +54,67 @@ public class LoginUseCaseSyncTest {
 
     //if login fails auth token is not changed
     @Test
-    public void loginSync_generalError_authTokennotCached(){
-mLoginHttpEndpointSyncTestDouble.isGeneralError = true;
+    public void loginSync_generalError_authTokennotCached() {
+        mLoginHttpEndpointSyncTestDouble.isGeneralError = true;
         mLoginUseCaseSync.loginSync(USERNAME, PASSWORD);
         assertThat(mAuthTokenCacheTestDouble.getAuthToken(), is(""));
     }
 
     @Test
-    public void loginSync_authError_authTokennotCached(){
+    public void loginSync_authError_authTokennotCached() {
         mLoginHttpEndpointSyncTestDouble.isAuthError = true;
         mLoginUseCaseSync.loginSync(USERNAME, PASSWORD);
         assertThat(mAuthTokenCacheTestDouble.getAuthToken(), is(""));
     }
 
     @Test
-    public void loginSync_serverError_authTokennotCached(){
+    public void loginSync_serverError_authTokennotCached() {
         mLoginHttpEndpointSyncTestDouble.isServerError = true;
         mLoginUseCaseSync.loginSync(USERNAME, PASSWORD);
         assertThat(mAuthTokenCacheTestDouble.getAuthToken(), is(""));
     }
 
 
-
-
     //if login succeeds login event posted to event bus
-
+    @Test
+    public void loginSync_serverError_loggedInEventPosted() {
+        mLoginUseCaseSync.loginSync(USERNAME, PASSWORD);
+        assertThat(mEventBusPosterTestDouble.mEvent, is(instanceOf(LoggedInEvent.class)));
+    }
 
     //if login fails no login event posted to event bus
+    @Test
+    public void loginSync_serverError_noInteractionWithEventBusPoster() {
+        mLoginHttpEndpointSyncTestDouble.isGeneralError = true;
+        mLoginUseCaseSync.loginSync(USERNAME, PASSWORD);
+        assertThat(mEventBusPosterTestDouble.mIntectionsCount, is(0));
+    }
 
     //if login succeeds success returns
 
+    @Test
+    public void loginSync_success_failureReturned() {
+       LoginUseCaseSync.UseCaseResult result = mLoginUseCaseSync.loginSync(USERNAME, PASSWORD);
+        assertThat(result, is(LoginUseCaseSync.UseCaseResult.SUCCESS));
+    }
+
     //if login fails failreturned
+    @Test
+    public void loginSync_failure_failureReturned() {
+        mLoginHttpEndpointSyncTestDouble.isServerError = true;
+        LoginUseCaseSync.UseCaseResult result = mLoginUseCaseSync.loginSync(USERNAME, PASSWORD);
+        assertThat(result, is(LoginUseCaseSync.UseCaseResult.FAILURE));
+    }
 
     //network - network error returned
+
+
+    @Test
+    public void loginSync_networkError_networkErrorReturned() {
+        mLoginHttpEndpointSyncTestDouble.mIsNetworkError = true;
+        LoginUseCaseSync.UseCaseResult result = mLoginUseCaseSync.loginSync(USERNAME, PASSWORD);
+        assertThat(result, is(LoginUseCaseSync.UseCaseResult.NETWORK_ERROR));
+    }
 
     /*
     This is a mix between  mock and stub test double.
@@ -97,18 +129,21 @@ mLoginHttpEndpointSyncTestDouble.isGeneralError = true;
         public boolean isGeneralError;
         public boolean isAuthError;
         public boolean isServerError;
+        public boolean mIsNetworkError;
 
         @Override
         public EndpointResult loginSync(String username, String password) throws NetworkErrorException {
             mUsername = username;
             mPassword = password;
-            if(isGeneralError){
-                return new EndpointResult(EndpointResultStatus.GENERAL_ERROR,"");
-            }else if (isAuthError){
-                return new EndpointResult(EndpointResultStatus.AUTH_ERROR,"");
-            } else if (isServerError){
-                return new EndpointResult(EndpointResultStatus.SERVER_ERROR,"");
-            }else{
+            if (isGeneralError) {
+                return new EndpointResult(EndpointResultStatus.GENERAL_ERROR, "");
+            } else if (isAuthError) {
+                return new EndpointResult(EndpointResultStatus.AUTH_ERROR, "");
+            } else if (isServerError) {
+                return new EndpointResult(EndpointResultStatus.SERVER_ERROR, "");
+            }else if (mIsNetworkError) {
+                throw new NetworkErrorException();
+            } else {
                 return new EndpointResult(EndpointResultStatus.SUCCESS, AUTH_TOKEN);
             }
 
@@ -135,9 +170,13 @@ mLoginHttpEndpointSyncTestDouble.isGeneralError = true;
 
     private static class EventBusPosterTestDouble implements EventBusPoster {
 
+        Object mEvent;
+        public int mIntectionsCount = 0;
+
         @Override
         public void postEvent(Object event) {
-
+            mIntectionsCount++;
+            mEvent = event;
         }
     }
 
